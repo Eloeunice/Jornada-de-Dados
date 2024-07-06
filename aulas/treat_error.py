1fcc12a6064a5b141f8d5dc6716a95f0705a8924
Eloiza <eloizaeunice25@gmail.com>
2024-07-06 18:23:30 -0400

    Acrescentando arquivos das aulas

---
+++ aulas/treat_error.py
@@ -0,0 +1,63 @@
+#EXERCÍCIOS
+
+  #Inteiros (int)
+#Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+num1 = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+sum = num1 + num2
+print(sum)
+
+#Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+num3 = int(input('Digite um número e eu te mostarei o resto da divisão desse número por 5:'))
+resto = num3 % 5
+print(resto)
+
+#Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+num4 = int(input('Digite o primeiro número:'))
+num5 = int(input('Digite o segundo número:'))
+mult = num4 * num5
+print(mult)
+
+#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+num6 = int(input('Digite o primeiro número:'))
+num7 = int(input('Digite o segundo número:'))
+divisao_inteira = num6 // num7
+print(divisao_inteira)
+
+#Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+num8 = int(input('Digite um número e eu te mostarei oo quadrado desse número:'))
+square = num8**2
+print(square)
+
+  #Números de Ponto Flutuante (float)
+#Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+#Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+#Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+#Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+#Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+  #Strings (str)
+#Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+#Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+#Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+#Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+#Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+  #Booleanos (bool)
+#Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+
+#Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+#Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+#Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+#Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
