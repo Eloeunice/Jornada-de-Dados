7a247ad2a5414a60d1791cc18a6f2c37e063dca9
Eloiza <eloizaeunice25@gmail.com>
2024-07-06 18:55:20 -0400

    exercicios de float resolvidos

---
+++ README.md
@@ -0,0 +1,1 @@
+Curso Jornada de Dados
--- aulas/treat_error.py
+++ aulas/treat_error.py
@@ -1,5 +1,5 @@
 #EXERCÍCIOS
-
+import math
   #Inteiros (int)
 #Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 num1 = int(input('Digite o primeiro número:'))
@@ -25,21 +25,35 @@ divisao_inteira = num6 // num7
 print(divisao_inteira)
 
 #Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
-num8 = int(input('Digite um número e eu te mostarei oo quadrado desse número:'))
+num8 = int(input('Digite um número e eu te mostarei o quadrado desse número:'))
 square = num8**2
 print(square)
 
   #Números de Ponto Flutuante (float)
 #Escreva um programa que receba dois números flutuantes e realize sua adição.
+num9 = float(input('Digite o primeiro número:'))
+num10 = float(input('Digite o segundo número:'))
+sum_float = num9 + num10
+print(sum_float)
 
 #Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
-
+num11 = float(input('Digite o primeiro número:'))
+num12 = float(input('Digite o segundo número:'))
+media = (num11 + num12) / 2
+print(media)
 #Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+base = float(input('Digite o número base:'))
+exp = float(input('Digite o número expoente :'))
+pot = base**exp
+print(pot)
 
 #Faça um programa que converta a temperatura de Celsius para Fahrenheit.
-
+temp_celsius = float(input("Digite a temperatura em celsius:"))
+temp_f = temp_celsius * 1,8 + 32
+print(temp_f)
 #Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-
+raio = float(input("Escreva o número do raio:"))
+area = math.pi * raio **2
   #Strings (str)
 #Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
