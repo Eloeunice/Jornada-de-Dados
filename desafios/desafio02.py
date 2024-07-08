56dd0034be6bace285fd286c5e1e91cf77864b8d
Eloiza <eloizaeunice25@gmail.com>
2024-07-08 09:07:24 -0400

    desafio02 resolvido

--- desafios/desafio02.py
+++ desafios/desafio02.py
@@ -2,14 +2,25 @@
 
 # 1) Solicita ao usuário que digite seu nome
 nome = input('Digite o seu nome:')
+if nome.isnumeric():
+    print("Nome inválido!")
+    exit()
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
-salario = float(input("Digite seu salario:"))
+try:
+    salario = float(input("Digite seu salario:"))
+except:
+    print("Valor inválido!")
+    exit()  # tem que sair e encerrar o programa se não ele vai continuar sem os dados
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
-bonus = float(input('Digite o bonus em porcentagem:'))
+try:
+    bonus = float(input('Digite o bonus em porcentagem:'))
+except:
+    print("Valor inválido!")
+    exit()
 
 # 4) Calcule o valor do bônus final
 valor_final = salario + salario * bonus
@@ -19,4 +30,3 @@ print(f'Olá {nome}, o seu salário com o bônus é de {valor_final:.2f} reais')
 
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
      #O usuário pode digitar um número no nome
-     #O bonus pode acabar sendo maior que o salario
